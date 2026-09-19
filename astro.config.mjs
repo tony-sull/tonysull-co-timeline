8e08be1097eff47bb265f895154bb6e0aa7b9c74
Tony Sullivan <dev@tonysull.co>
2026-09-19 10:57:07 -0500

    send from login@

--- astro.config.mjs
+++ astro.config.mjs
@@ -43,7 +43,7 @@ export default defineConfig({
 
       subscriptions: {
         enabled: true,
-        fromEmail: 'dev@tonysull.co',
+        fromEmail: 'login@tonysull.co',
       },
     }),
     indiepubAdmin(),
